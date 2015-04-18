44fb62b7d367f1733d3a6c5623f4a6f1566e880a
Nathan Borror <nathan@nathanborror.com>
2015-04-18 12:32:20 -0700

    Adding ReturnJSON helper method

--- render/helpers.go
+++ render/helpers.go
@@ -1,10 +1,20 @@
 package render
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
+// Check is a shorthand function for error checking
 func Check(err error, w http.ResponseWriter) {
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+}
+
+// ReturnJSON returns json
+func ReturnJSON(context map[string]interface{}) string {
+	obj, _ := json.MarshalIndent(context, "", "  ")
+	return string(obj[:])
 }
