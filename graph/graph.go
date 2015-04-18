00ded7a56f9b326e6c5344a1373b9ddf4b02953c
Nathan Borror <nathan@nathanborror.com>
2015-04-18 13:00:30 -0700

    Cleanup

--- graph/graph.go
+++ graph/graph.go
@@ -1,3 +1,24 @@
+// Package graph package is a very simplistic implementation of Facebook's GraphQL.
+// This is a very naive implementation and doesn't by any means support all the
+// great features GraphQL offers.
+//
+// Usage
+//
+//    query := "book(1){notes,editions}"
+//
+//   	objects := ObjectMap{
+//   		"book":    Book{},
+//   		"edition": Edition{},
+//   		"note":    Note{},
+//   	}
+//
+//    lists := ObjectListMap{
+//   		"books":    []*Book{},
+//   		"editions": []*Edition{},
+//   		"notes":    []*Note{},
+//   	}
+//
+//    graph.Query(query, objects, lists)
 package graph
 
 import (
@@ -10,38 +31,22 @@ import (
 	"github.com/nathanborror/gommon/settings"
 )
 
+// ObjectMap is a map of all available objects
+type ObjectMap map[string]interface{}
+
+// ObjectListMap is a map of all available object lists
+type ObjectListMap map[string]interface{}
+
 func unpack(s []string, vars ...*string) {
 	for i, str := range s {
 		*vars[i] = str
 	}
 }
 
-/*
-  Query lets you use a GraphQL like syntax to query your database. For example
-  http://localhost?q=book(1){notes,editions} would return a JSON response with
-  a book object and a list of notes and editions that match the book's key.
-
-  Usage:
-
-      query := "book(1){notes,editions}"
-
-    	types := map[string]interface{}{
-    		"book":      &Book{},
-    		"edition":   &Edition{},
-    		"note":      &Note{},
-    	}
-
-    	lists := map[string]interface{}{
-    		"books":      &[]*Book{},
-    		"editions":   &[]*Edition{},
-    		"notes":      &[]*Note{},
-    	}
-
-      graph.Query(query, types, lists)
-
-*/
-
-func Query(query string, types map[string]interface{}, lists map[string]interface{}) map[string]interface{} {
+// Query lets you use a GraphQL like syntax to query your database. For example
+// http://localhost?q=book(1){notes,editions} would return a JSON response with
+// a book object and a list of notes and editions that match the book's key.
+func Query(query string, objects ObjectMap, lists ObjectListMap) map[string]interface{} {
 
 	// Database
 	settings := settings.NewSettings("settings.json")
@@ -59,23 +64,23 @@ func Query(query string, types map[string]interface{}, lists map[string]interfac
 	context := map[string]interface{}{}
 
 	// Retrieve primary node
-	obj := types[node]
+	obj := objects[node]
 	statement := fmt.Sprintf("SELECT * FROM %s WHERE key = '%s'", node, key)
-	err = db.Get(obj, statement)
+	err = db.Get(&obj, statement)
 	context[node] = obj
 
 	// Retrieve children
 	if rawChildren != "" {
 		children := strings.Split(rawChildren, ",")
 		for _, child := range children {
-			objList := lists[child]
+			list := lists[child]
 
 			re := regexp.MustCompile(`s$`)
 			table := re.ReplaceAllString(child, "")
 
 			statement := fmt.Sprintf("SELECT * FROM %s WHERE %s = '%s'", table, node, key)
-			err = db.Select(objList, statement)
-			context[child] = objList
+			err = db.Select(&list, statement)
+			context[child] = list
 		}
 	}
 
